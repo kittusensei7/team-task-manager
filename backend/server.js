ba68d9c90cc75a62d76c66b5374dd879d7b8152b
kittusensei7 <easarapujagadeesh17@gmail.com>
2026-05-07 22:05:15 +0530

    fixed dependencies

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected successfully');
+    const PORT = process.env.PORT || 5000
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
